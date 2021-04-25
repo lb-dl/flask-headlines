736959ec22b19430c7391f39b28e029ff7f52f22
SKyzjBpN <sensitivestory@gmail.com>
2021-04-25 15:36:31 +0300

    Adding currency data

--- headlines.py
+++ headlines.py
@@ -5,8 +5,8 @@ import requests
 from flask import Flask, render_template, request
 
 
-
 app = Flask(__name__)
+
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
@@ -15,8 +15,12 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              }
 
 DEFAULTS = {'publication': 'bbc',
-            'city': 'Dnipro, Ukraine'}
+            'city': 'Dnipro, Ukraine',
+            'currency_from': 'UAH',
+            'currency_to': 'USD'}
 
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}"
+CURRENCY_URL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
 
 @app.route('/')
 def home():
@@ -31,8 +35,17 @@ def home():
     if not city:
         city = DEFAULTS['city']
     weather = get_weather(city)
-    return render_template("home.html", articles=articles, weather=weather)
-
+    # get customized currency based on user input or default
+    currency_from = request.args.get("currency_from")
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    currency_to = request.args.get("currency_to")
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rate = get_rate(currency_to)
+    return render_template("home.html", articles=articles,
+                           weather=weather, currency_to=currency_to,
+                           currency_from=currency_from, rate=rate)
 
 
 def get_news(query):
@@ -50,10 +63,9 @@ def get_api_key():
     return config['openweathermap']['api']
 
 
-
 def get_weather(query):
     api_key = get_api_key()
-    url_api = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}".format(query, api_key)
+    url_api = WEATHER_URL.format(query, api_key)
     data = requests.get(url_api)
     parsed = data.json()
     weather = None
@@ -63,6 +75,17 @@ def get_weather(query):
              'temperature': parsed['main']['temp'],
              'city': parsed['name'], 'country': parsed['sys']['country']}
     return weather
+
+
+def get_rate(to):
+    data = requests.get(CURRENCY_URL)
+    parsed = data.json()
+    for row in parsed:
+        if row['ccy'] == to:
+            buy = row['buy']
+            sell = row['sale']
+
+            return f'{buy}/{sell}'
 
 
 
