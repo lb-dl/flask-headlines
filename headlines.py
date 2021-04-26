672c5936a2ad64cf055587619a49db8bf15b4f50
SKyzjBpN <sensitivestory@gmail.com>
2021-04-26 17:56:12 +0300

    Adding cookies

--- headlines.py
+++ headlines.py
@@ -1,8 +1,9 @@
 import configparser
+import datetime
 import feedparser
 import requests
 
-from flask import Flask, render_template, request
+from flask import Flask, make_response, render_template, request
 
 
 app = Flask(__name__)
@@ -25,27 +26,32 @@ CURRENCY_URL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5
 @app.route('/')
 def home():
     # get customized headlines, based on user input or default
-    publication = request.args.get('publication')
-    if not publication:
-        publication = DEFAULTS['publication']
+    publication = get_value_with_fallback("publication")
     articles = get_news(publication)
     # get customized weather based on user input or default
-    city = request.args.get('city')
-    print(f'city: {city}')
-    if not city:
-        city = DEFAULTS['city']
+    city = get_value_with_fallback("city")
     weather = get_weather(city)
     # get customized currency based on user input or default
-    currency_from = request.args.get("currency_from")
-    if not currency_from:
-        currency_from = DEFAULTS['currency_from']
-    currency_to = request.args.get("currency_to")
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
+    currency_from = get_value_with_fallback("currency_from")
+    currency_to = get_value_with_fallback("currency_to")
     rate = get_rate(currency_to)
-    return render_template("home.html", articles=articles,
+    response = make_response(render_template("home.html", articles=articles,
                            weather=weather, currency_to=currency_to,
-                           currency_from=currency_from, rate=rate)
+                           currency_from=currency_from, rate=rate))
+    expires = datetime.datetime.now() + datetime.timedelta(days=365)
+    response.set_cookie("publication", publication, expires=expires)
+    response.set_cookie("city", city, expires=expires)
+    response.set_cookie("currency_to", currency_to, expires=expires)
+    return response
+
+
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
+
 
 
 def get_news(query):
@@ -54,13 +60,13 @@ def get_news(query):
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return feed['entries']
+    return feed["entries"]
 
 
 def get_api_key():
     config = configparser.ConfigParser()
-    config.read('config.ini')
-    return config['openweathermap']['api']
+    config.read("config.ini")
+    return config["openweathermap"]["api"]
 
 
 def get_weather(query):
@@ -69,11 +75,11 @@ def get_weather(query):
     data = requests.get(url_api)
     parsed = data.json()
     weather = None
-    if parsed.get('weather'):
+    if parsed.get("weather"):
         weather = \
-            {'description': parsed['weather'][0]['description'],
-             'temperature': parsed['main']['temp'],
-             'city': parsed['name'], 'country': parsed['sys']['country']}
+            {"description": parsed["weather"][0]["description"],
+             "temperature": parsed["main"]["temp"],
+             "city": parsed["name"], "country": parsed["sys"]["country"]}
     return weather
 
 
@@ -81,9 +87,9 @@ def get_rate(to):
     data = requests.get(CURRENCY_URL)
     parsed = data.json()
     for row in parsed:
-        if row['ccy'] == to:
-            buy = row['buy']
-            sell = row['sale']
+        if row["ccy"] == to:
+            buy = row["buy"]
+            sell = row["sale"]
 
             return f'{buy}/{sell}'
 
